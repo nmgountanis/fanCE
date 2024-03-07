602a6e49560bfc6edbc923b1cb4261f8b35ab9a1
nmgountanis <143458646+nmgountanis@users.noreply.github.com>
2024-03-07 16:57:50 -0500

    Update fanCE_plot.py

--- fanCE_plot.py
+++ fanCE_plot.py
@@ -35,7 +35,7 @@ def plotSFH(t,sfr,dtout,labels,axes=None):
     Plots normalized star formation rate versus time
     
     Input arguments:
-      t = 1-d time array
+      t = 1-d time array for desired outputs
       sfr = star formation rate, 1-d array evaluated at the times in t
       labels = string label
     
@@ -69,7 +69,7 @@ def plotGas(t,MgH,FeH,MgFe,labels,axes=None):
     [O/H] and [O/Fe] are identical to [Mg/H] and [Mg/Fe].
     
     Input arguments:
-      t = 1-d time array
+      t = 1-d time array for desired outputs
       MgH = [Mg/H], 1-d array evaluated at the times in t
       FeH = [Fe/H], 1-d array evaluated at the times in t
       MFe = [Mg/Fe], 1-d array evaluated at the times in t
@@ -104,4 +104,63 @@ def plotGas(t,MgH,FeH,MgFe,labels,axes=None):
     ax[2].legend(fontsize="18", loc = "upper right")
     
     plt.tight_layout()
+    
+def plotStellar(t,sfr,dtout,mean_age,MgH,MgFe,MgHStar,MgFeStar,labels):
+    
+    fig, ax = plt.subplots(nrows=4, ncols=2, figsize=(11, 18))
+    plt.style.use('seaborn-v0_8-colorblind')
+
+    linew = 3
+
+    ax[0,0].plot(t, sfr/p.dtout, c = "C2", linewidth=linew)
+    ax[0,0].set_xlabel('time (Gyr)', labelpad = -2)
+    ax[0,0].set_ylabel('SFR')
+    ax[0,0].set_xlim([-0.5, 14.5])
+
+    ax[0,1].plot(t, mean_age, c = "C2", label = labels,linewidth=linew)
+    ax[0,1].set_xlabel('time (Gyr)', labelpad = -2)
+    ax[0,1].set_ylabel('$\langle$age$\\rangle$ (Gyr)')
+    ax[0,1].set_xlim([-0.5, 14.5])
+    ax[0,1].yaxis.set_ticks([5,10,15])
+    ax[0,1].legend(fontsize="16", loc = "upper right")
+
+    ax[1,0].plot(t, MgH, c = "C2",linewidth=linew)
+    ax[1,0].set_xlabel('time (Gyr)', labelpad = -2)
+    ax[1,0].set_ylabel('[Mg/H]')
+    ax[1,0].set_xlim([-0.5, 14.5])
+    ax[1,0].set_ylim([-2.5, .75])
+
+    ax[1,1].plot(t, MgHStar, c = "C2", linewidth=linew)
+    ax[1,1].set_xlabel('time (Gyr)', labelpad = -2)
+    ax[1,1].set_ylabel('$\langle$[Mg/H]$\\rangle$')
+    ax[1,1].set_xlim([-0.5, 14.5])
+    ax[1,1].set_ylim([-2.5, .75])
+
+    ax[2,0].plot(t, MgFe,c = "C2",linewidth=linew)
+    ax[2,0].set_xlabel('time (Gyr)', labelpad = -2)
+    ax[2,0].set_ylabel('[Mg/Fe]', labelpad = -15)
+    ax[2,0].set_xlim([-0.5, 14.5])
+    ax[2,0].set_ylim([-.2, .575])
+
+    ax[2,1].plot(t, MgFeStar, c = "C2", linewidth=linew)
+    ax[2,1].set_xlabel('time (Gyr)', labelpad = -2)
+    ax[2,1].set_ylabel('$\langle$[Mg/Fe]$\\rangle$', labelpad = -15)
+    ax[2,1].set_xlim([-0.5, 14.5])
+    ax[2,1].set_ylim([-.2, .575])
+
+    ax[3,0].plot(mean_age, MgHStar, c = "C2",linewidth=linew)
+    ax[3,0].set_xlabel('$\langle$age$\\rangle$ (Gyr)', labelpad = -1)
+    ax[3,0].set_ylabel('$\langle$[Mg/H]$\\rangle$')
+    ax[3,0].set_xlim([1, 14.5])
+    ax[3,0].set_ylim([-2.5, .75])
+
+    ax[3,1].plot(mean_age, MgFeStar, c = "C2", linewidth=linew)
+    ax[3,1].set_xlabel('$\langle$age$\\rangle$ (Gyr)', labelpad = -1)
+    ax[3,1].set_ylabel('$\langle$[Mg/Fe]$\\rangle$', labelpad = -15)
+    ax[3,1].set_xlim([1, 14.5])
+    ax[3,1].set_ylim([-.2, .5])
+
+    plt.tight_layout()
+    fig.subplots_adjust(hspace=.28)
+    fig.subplots_adjust(wspace=.3)
     
