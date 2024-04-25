eed3f7ddefe4d37c05f0e53c8d1bd9d422d634f4
nathansandford <nathan_sandford@berkeley.edu>
2024-04-25 12:43:06 -0400

    refactored stellar property weighting

--- fance/stellar.py
+++ fance/stellar.py
@@ -1,71 +1,40 @@
 import numpy as np
 
 
-def weight(t,t0):
-    
+def weight_stellar_props(
+        t: np.ndarray,
+        t0: float,
+        sfr: np.ndarray,
+        OH: np.ndarray,
+        MgH: np.ndarray,
+        FeH: np.ndarray,
+        light_weighted: bool = True,
+):
     """
-    Compute weighting function
-    
-    Input arguments (illustrative values in [  ]):
-      t = time array for desired outputs [0.0,0.02,0.04, ..., 12.0]
-      t0 = age of universe at observed redshift [14]
-           t0=0 returns weight=1, corresponding to mass-weighting
+    Compute light-/mass-weighted, log-averaged age and abundances.
 
-    Returns:
-      weighting function, a 1-d array evaluated at the times in t. 
-
-    Time unit is Gyr
-    
+    :param np.ndarray t: Model output (not input!) time array (in Gyr).
+    :param float t0: Age of Universe at observed redshift [14]
+    :param np.ndarray sfr: Star formation rate at times in t
+    :param np.ndarray OH: Stellar [O/H] at times in t
+    :param np.ndarray MgH: Stellar [Mg/H] at times in t
+    :param np.ndarray FeH: Stellar [Fe/H] at times in t
+    :param light_weighted: Use light weighting from Gountanis+24 Eq. 1.
+        If false, outputs are mass-weighted.
+    :return: Tuple[float, float, float, float, float, float]:
+        mean_age, OHStar, MgHStar, FeHStar, OFeStar, MgFeStar --- each a 1-d array corresponding to the time array
     """
-    
-    if t0 == 0:
-        return 1                               # weight=1 corresponds to mass-weighting
-    
-    
-    else:
-        return 1.5 * ((t0 - t + 0.1)**-0.9)   # light-weighting based off Gountanis+24 eq. 1
-    
-    
-    
-def stellar(t,t0,sfr,weight,OH,MgH,FeH,OFe,MgFe):
-    
-    """
-    Compute light-weighted, log-averaged age and abundances.
-    
-    Input arguments (illustrative values in [  ]):
-      t      = time array for desired outputs [0.0,0.02,0.04, ..., 12.0]
-      t0     = age of universe at observed redshift [14]
-      sfr    = star formation rate [4.03277751e-05,...7.50721612e-04]
-      weight = light-weighting [0.19607826, 0.1963038, ..., 9.03839379]
-      OH     = [O/H] evaluated at the times in t
-      MgH    = [Mg/H] evaluated at the times in t
-      FeH    = [Fe/H] evaluated at the times in t
-      OFe    = [O/Fe] evaluated at the times in t
-      MFe    = [Mg/Fe] evaluated at the times in t
 
-    Returns:
-      mean_age,OHStar,MgHStar,FeHStar,OFeStar, MgFeStar
-      each a 1-d array evaluated at the times in t
-
-    Time unit is Gyr
-    
-    """
-                
-    OHStar = np.cumsum(OH*sfr*weight)/np.cumsum(weight*sfr)   # light-weighted, log-averaged [O/H]
-    
-    MgHStar = np.cumsum(MgH*sfr*weight)/np.cumsum(weight*sfr) # light-weighted, log-averaged [Mg/H]
-    
-    FeHStar = np.cumsum(FeH*sfr*weight)/np.cumsum(weight*sfr) # light-weighted, log-averaged [Fe/H]
-            
-    OFeStar = OHStar - FeHStar                                # light-weighted, log-averaged [O/Fe]
-    
-    MgFeStar = MgHStar - FeHStar                              # light-weighted, log-averaged [Mg/Fe]
-                
-    
-    # light-weighted, log-averaged age
-    mean_age = 10**(np.cumsum(np.log10(t0-t+1e-5)*sfr*weight)/np.cumsum(sfr*weight)) 
-    
-    return mean_age,OHStar,MgHStar,FeHStar,OFeStar,MgFeStar
-    
-    
+    if light_weighted:  # light-weighting based off Gountanis+24 Eq. 1
+        weights = 1.5 * ((t0 - t + 0.1)**-0.9)
+    else:  # mass weighted
+        weights = np.ones_like(t)
+    OHStar = np.cumsum(OH * sfr * weights) / np.cumsum(weights * sfr)
+    MgHStar = np.cumsum(MgH * sfr * weights) / np.cumsum(weights * sfr)
+    FeHStar = np.cumsum(FeH * sfr * weights) / np.cumsum(weights * sfr)
+    OFeStar = OHStar - FeHStar
+    MgFeStar = MgHStar - FeHStar
+    # light-/mass-weighted, log-averaged age
+    mean_age = 10**(np.cumsum(np.log10(t0 - t + 1e-5) * sfr * weights) / np.cumsum(sfr * weights))
+    return mean_age, OHStar, MgHStar, FeHStar, OFeStar, MgFeStar
      
